c18d9a9d2c18cdd6c65916ee4bd985ea68b513e9
carmowa <victorcarmona1802@gmail.com>
2024-10-27 23:48:28 -0300

    Footer quase pronto.

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+
+export default function Footer() {
+    return (
+        <footer className="text-center">
+            <p>
+                Desenvolvido por Ricardo, Pedro e Victor
+            </p>
+        </footer>
+    )
+}
